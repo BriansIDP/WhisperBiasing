cf1d97b33be9f120a88ed35c6a1a38fe4f8b1eea
G. Sun <gs534@login-e-1.data.cluster>
2023-02-22 23:02:04 +0000

    temperal change

--- decoding.sh
+++ decoding.sh
@@ -1,17 +1,19 @@
 . /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet
-expdir=exp/finetune_librispeech_lr0.0005_KB200_drop0.1
-decodedir=decode_no_lm_b10_KB1000_50best
+# expdir=exp/finetune_librispeech_lr0.0005_KB200_drop0.1
+expdir=exp/LibriSpeech_large_origmodel
+decodedir=decode_no_lm_b10_KB1000
 mkdir -p $expdir/$decodedir
 python decode.py \
     --test_json data/LibriSpeech/test_clean_full.json \
-    --beamsize 50 \
+    --beamsize 10 \
     --expdir $expdir/$decodedir \
-    --loadfrom $expdir/model.acc.best \
-    --biasing \
-    --biasinglist data/LibriSpeech/Blist/all_rare_words.txt \
-    --dropentry 0.0 \
-    --maxKBlen 1000 \
-    --save_nbest \
+    --modeltype large \
+    # --loadfrom $expdir/model.acc.best \
+    # --biasing \
+    # --biasinglist data/LibriSpeech/Blist/all_rare_words.txt \
+    # --dropentry 0.0 \
+    # --maxKBlen 1000 \
+    # --save_nbest \
     # --use_gpt2 \
     # --lm_weight 0.01 \
     # --ilm_weight 0.005 \
