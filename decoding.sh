23077bbe2091e0843b3a6ad18df9da3dfd4e2bc5
G. Sun <gs534@login-e-3.data.cluster>
2023-05-18 20:41:49 +0100

    decoding script

--- decoding.sh
+++ decoding.sh
@@ -1,19 +1,18 @@
 . /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet
-# expdir=exp/finetune_librispeech_lr0.0005_KB200_drop0.1
-expdir=exp/LibriSpeech_large_origmodel
-decodedir=decode_no_lm_b10_KB1000
+expdir=exp/finetune_librispeech_lr0.0005_KB200_drop0.1
+testset=other
+decodedir=decode_no_lm_b50_KB1000_${testset}_50best
 mkdir -p $expdir/$decodedir
 python decode.py \
-    --test_json data/LibriSpeech/test_clean_full.json \
-    --beamsize 10 \
+    --test_json data/LibriSpeech/test_${testset}_full.json \
+    --beamsize 50 \
     --expdir $expdir/$decodedir \
-    --modeltype large \
-    # --loadfrom $expdir/model.acc.best \
-    # --biasing \
-    # --biasinglist data/LibriSpeech/Blist/all_rare_words.txt \
-    # --dropentry 0.0 \
-    # --maxKBlen 1000 \
-    # --save_nbest \
+    --loadfrom $expdir/model.acc.best \
+    --biasing \
+    --biasinglist data/LibriSpeech/Blist/all_rare_words.txt \
+    --dropentry 0.0 \
+    --maxKBlen 1000 \
+    --save_nbest \
     # --use_gpt2 \
     # --lm_weight 0.01 \
     # --ilm_weight 0.005 \
