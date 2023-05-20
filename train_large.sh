d4f95c4c2847353b88a50c9a150d21c74ae3c89c
G. Sun <gs534@login-e-3.data.cluster>
2023-05-20 07:46:34 +0100

    training script

---
+++ train_large.sh
@@ -0,0 +1,21 @@
+. /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet
+expdir=finetune_large_librispeech_lr0.0005_KB100_drop0.3
+mkdir -p exp/${expdir}
+python train.py \
+    --modeltype large \
+    --train_json data/LibriSpeech/train_clean_100_error.json \
+    --dev_json data/LibriSpeech/dev_error.json \
+    --lr 0.0005 \
+    --batch_size 8 \
+    --log_interval 200 \
+    --nepochs 30 \
+    --warmup_pct 0.0 \
+    --decay_pct 0.2 \
+    --expdir exp/${expdir} \
+    --logfile exp/${expdir}/log.txt \
+    --accumgrad 10 \
+    --biasing \
+    --biasinglist data/LibriSpeech/Blist/rareword_error.txt \
+    --dropentry 0.3 \
+    --maxKBlen 100 \
+    --loadfrom exp/${expdir}/model.acc.best \
